1c72c7a133dc3f9a667b0c905c7cb642f34ee57e
abdifitahabdulkadir <abdifitahabdulkadir22@gmail.com>
2026-06-01 15:50:44 +0300

    set up auth pages

---
+++ lib/schema.ts
@@ -0,0 +1,21 @@
+import z from "zod";
+
+
+export const RegisterSchema =  z.object({
+  email: z.email("Please provide valid Email address"),
+  password: z.
+    string().min(1, "Password is required").
+    max(20, "Passoword must be less than 20 characters.").
+    regex(/^[A-Za-z][A-Za-z0-9]*$/),
+  name: z.string().min(1, "Name is required").max(30,"Name must be less than 30 characters"),
+})
+
+export const LoginSchema = RegisterSchema.omit({
+  name:true
+})
+
+
+
+// types
+export  type   RegisterSchemaType = z.infer<typeof RegisterSchema>
+export  type   LoginSchemaType = z.infer<typeof LoginSchema>
